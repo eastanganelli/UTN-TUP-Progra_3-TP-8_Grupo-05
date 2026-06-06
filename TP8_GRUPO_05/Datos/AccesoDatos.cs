3c40fcb0e06b22ab909de1f03259917b6a515a87
Rocio Giusto <giustorocio@gmail.com>
2026-06-06 17:21:44 -0300

    Ej1 V5

--- TP8_GRUPO_05/Datos/AccesoDatos.cs
+++ TP8_GRUPO_05/Datos/AccesoDatos.cs
@@ -29,5 +29,32 @@ namespace Datos
             }
 
         }
+
+        public int ObtenerMaximo(String consulta)
+        {
+            int max = 0;
+            SqlConnection Conexion = ObtenerConexion();
+            SqlCommand cmd = new SqlCommand(consulta, Conexion);
+            SqlDataReader datos = cmd.ExecuteReader();
+            if (datos.Read())
+            {
+                max = Convert.ToInt32(datos[0].ToString());
+            }
+            return max;
+        }
+
+        public int EjecutarProcedimientoAlmacenado(SqlCommand Comando, String NombreSP)
+        {
+            int FilasCambiadas;
+            SqlConnection Conexion = ObtenerConexion();
+            SqlCommand cmd = new SqlCommand();
+            cmd = Comando;
+            cmd.Connection = Conexion;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = NombreSP;
+            FilasCambiadas = cmd.ExecuteNonQuery();
+            Conexion.Close();
+            return FilasCambiadas;
+        }
     }
 }
---
+++ TP8_GRUPO_05/Datos/DatosSucursal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using Entidades;
+
+namespace Datos
+{
+    internal class DatosSucursal
+    {
+        AccesoDatos acceso = new AccesoDatos();
+
+        public int agregarSucursal(Sucursal sucursal)
+        {
+            sucursal.IdSucursal = acceso.ObtenerMaximo("SELECT max(idSucursal) FROM Sucursales") + 1;
+            SqlCommand comando = new SqlCommand();
+            ArmarParametrosSucursalAgregar(ref comando, sucursal);
+            return acceso.EjecutarProcedimientoAlmacenado(comando, "spAgregarSucursal");
+        }
+
+        private void ArmarParametrosSucursalAgregar(ref SqlCommand Comando, Sucursal sucursal)
+        {
+            SqlParameter SqlParametros = new SqlParameter();
+            SqlParametros = Comando.Parameters.Add("@IDSUCURSAL", SqlDbType.Int);
+            SqlParametros.Value = sucursal.IdSucursal;
+            SqlParametros = Comando.Parameters.Add("@NOMBRESUCURSAL", SqlDbType.VarChar);
+            SqlParametros.Value = sucursal.Nombre_Sucursal;
+        }
+
+    }
+}
